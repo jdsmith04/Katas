d5714d72ab1f6a0e4fd1e33f8ce5dd082f7d8f93
smithjere14 <smithjere14@gmail.com>
2021-06-19 16:13:08 -0400

    simplified line 12 if statement

--- primes.py
+++ primes.py
@@ -2,22 +2,22 @@ from math import isqrt
 
 primes = [2]
 
-for integer in range(3, 1000):
+for integer in range(3, 100000):
     square_root = isqrt(integer)
     primes.append(integer)
     for prime_number in primes:
         if prime_number <= square_root:
-            print(f'integer square root: {square_root}')
-            print(f'{integer} / {prime_number} = {integer // prime_number} remainder {integer % prime_number}')
-            if integer // prime_number > 1 and integer % prime_number == 0:
-                print('NOT PRIME\n')
+            # print(f'integer square root: {square_root}')
+            # print(f'{integer} / {prime_number} = {integer // prime_number} remainder {integer % prime_number}')
+            if integer % prime_number == 0:
+                # print('NOT PRIME\n')
                 primes.remove(integer)
                 break
             if prime_number == square_root:
-                print('PRIME!!!\n')
+                # print('PRIME!!!\n')
                 break
         else:
-            print('PRIME!!!\n')
+            # print('PRIME!!!\n')
             break
 
 print(primes)
